52f3fdac6760933aad0044c25c200311cba2e4df
Quentin <neurotronic67@gmail.com>
2025-04-20 20:28:54 +0200

    need to test with new inertia orientation

--- mujoco_playground/experimental/sim2sim/play_bd5_joystick.py
+++ mujoco_playground/experimental/sim2sim/play_bd5_joystick.py
@@ -154,7 +154,7 @@ def load_callback(model=None, data=None):
   model.opt.timestep = sim_dt
 
   policy = OnnxController(
-      policy_path=(_ONNX_DIR / "BD5_REAL_GRAVITYONLY.onnx").as_posix(),
+      policy_path=(_ONNX_DIR / "bd5_PERFECT_policy.onnx").as_posix(),
       default_angles=np.array(model.keyframe("init_pose").qpos[7:]),
       ctrl_dt=ctrl_dt,
       n_substeps=n_substeps,
